b65808c2ffec6327d1fed74cfcfe739b12c3033d
cabrera-evil <douglascabrera.dev@gmail.com>
2024-08-08 12:10:12 -0600

    refactor: scripts auto linked

---
+++ init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Set the base directory to the current working directory
+base_dir=$(pwd)
+
+echo "Creating symbolic links for scripts in the '$base_dir' directory and its subdirectories..."
+
+# Function to create symbolic links for all scripts in the directory
+create_symlinks() {
+    local dir=$1
+    for file in "$dir"/*; do
+        if [ -f "$file" ] && [ -x "$file" ]; then
+            sudo ln -sf "$file" "/usr/local/bin/$(basename "$file")"
+        elif [ -d "$file" ]; then
+            create_symlinks "$file"
+        fi
+    done
+}
+
+# Start the process from the base directory
+create_symlinks "$base_dir"
+
+echo "Symbolic links creation completed."
